b9f69d28e47feda999538e3964404a27b39f4d44
greatlaki <greatlaki911@gmail.com>
2022-04-19 17:42:01 +0300

    Models were added

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Ticket(models.Model):
+
+    STATUS_CHOICES = [
+        ('OK', 'resolved'),
+        ('NO', 'unresolved'),
+        ('F', 'frozen'),
+    ]
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=250)
+    status = models.IntegerField(verbose_name='status of ticket', choices=STATUS_CHOICES, default=2)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Ticket'
+        verbose_name_plural = 'Tickets'
+        ordering = ['-created_date']
+
+
+class SupportResponse(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='Answer')
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
+        ordering = ['-created_date']
+
