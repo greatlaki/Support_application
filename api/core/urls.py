333d97b6a09098555d6b10ae9a2d3086bc309efd
greatlaki <greatlaki911@gmail.com>
2022-04-20 18:02:49 +0300

    Some changes with urls

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from .views import *
 
+router = DefaultRouter()
+router.register('tickets', TicketList, basename='tickets')
+
 urlpatterns = [
+    path("", include(router.urls)),
     path('append_tickets/', AppendTicket.as_view(), name='append_tickets'),
-    path('tickets_list/', TicketList.as_view(), name='tickets_list'),
     path('support_response/', ResponseList.as_view(), name='support_response'),
     path('details/', TicketDetail.as_view(), name='details'),
+    path('register/', RegisterView.as_view()),
 ]
